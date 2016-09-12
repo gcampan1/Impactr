11febd0bdf82a254265d47f14099ce3fa0f456cc
guy <gcampan1@villanova.edu>
2016-09-12 14:39:01 -0400

    Move code from app.py into api.py

--- api.py
+++ api.py
@@ -1,10 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, jsonify
+from schema import Schema
+
+from mongo import mongo
 
 api = Blueprint("api", __name__)
 
-@api.route("/players")
+player_schema = Schema({
+  "firstname": str,
+  "age": int,
+  "height": float,
+  "weight": float
+})
+
+impact_schema = Schema({
+  "player": str,
+  "data": float,
+  "dt": int
+})
+
+@api.route("/players", methods=["GET", "POST"])
 def players():
-    return "players"
+  if request.method == "POST":
+    # Validate data
+    data = request.json
+    try:
+      player_schema.validate(data)
+    except:
+      abort(400)
+    # Add a player
+    data["player_id"] = str(uuid4())
+    mongo.db.players.insert(data)
+    return Response("OK", status=200)
+  else:
+    # Retrieve a player
+    return jsonify(list(mongo.db.players.find({}, {"_id":0})))
 
 @api.route("/impact")
 def impact():
