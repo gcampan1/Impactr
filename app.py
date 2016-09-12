d3fa0ea58cb5f8d7a1df7e857abc20c411552a45
guy <gcampan1@villanova.edu>
2016-09-12 12:43:54 -0400

    Add support for POSTing players

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, Response
 from flask_pymongo import PyMongo
+from schema import Schema
+from uuid import uuid4
 
 app = Flask("Impactr")
 mongo = PyMongo(app)
 
-@app.route("/api/players")
+player_schema = Schema({
+  "name": str,
+  "age": int,
+  "height": float,
+  "weight": float
+})
+
+impact_schema = Schema({
+  "player": str,
+  "data": float,
+  "dt": int
+})
+
+@app.route("/api/players", methods=["GET", "POST"])
 def players():
-  return jsonify(list(mongo.db.players.find({}, {"_id":0})))
+  if request.method == "POST":
+    # Validate data
+    data = request.json
+    try:
+      player_schema.validate(data)
+    except:
+      abort(400)
+    # Add a player
+    data["player_id"] = str(uuid4())
+    mongo.db.players.insert(data)
+    return Response("OK", status=200)
+  else:
+    # Retrieve a player
+    return jsonify(list(mongo.db.players.find({}, {"_id":0})))
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
