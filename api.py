d1da199d6cbd1f0535a6470d0168010a7fb4fbf4
gcampan1 <gcampan1@villanova.edu>
2016-09-29 11:37:25 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,18 +6,33 @@ from mongo import mongo
 
 api = Blueprint("api", __name__)
 
+
 player_schema = Schema({
-  "firstname": str,
+  "name": str,
   "age": int,
   "height": float,
-  "weight": float
+  "weight": float,
+  #"MAC_addr": str
 })
 
 impact_schema = Schema({
-  "player": str,
-  "data": float,
-  "dt": int
+  "player_id": str,
+  "date": str,
+  "time": str,
+  "a_x": float,
+  "a_y": float,
+  "a_z": float,
+  "roll": float,
+  "pitch": float,
+  "yaw": float,
+  "ug_x": float,
+  "ug_y": float,
+  "ug_z": float,
+  "o2": int,
+  "hyd": int,
+  "bpm": int
 })
+
 
 @api.route("/players", methods=["GET", "POST"])
 def players():
@@ -36,6 +51,21 @@ def players():
     # Retrieve a player
     return jsonify(list(mongo.db.players.find({}, {"_id":0})))
 
-@api.route("/impact")
+
+
+@api.route("/impact", methods=["GET", "POST"])
 def impact():
-    return "impact"
+  if request.method == "POST":
+    #Validate data
+    data = request.json
+    try:
+      impact_schema.validate(data)
+    except:
+      abort(400)
+    #Add Impact data
+    #data["impact_data"] = mongo.db.players.find_one({"_id":0})
+    mongo.db.impact.insert(data) 
+    return Response("OK", status=200)
+  else:
+    #Retrieve Impact Data
+    return jsonify(list(mongo.db.impact.find({}, {"_id":0})))
