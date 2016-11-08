1745c07ffc39ce53233c4c87066f1cfec0869c8d
Brian Kim <bk@breadtech.com>
2016-11-08 09:13:26 -0800

    Refactor routes for cleaner usage

--- www.py
+++ www.py
@@ -10,30 +10,14 @@ def home():
 def players():
     return render_template("players.html")
 
+@www.route("/players/<username>")
+def player_by_id(username):
+    return render_template("player_data.html")
+
 @www.route("/players/new")
 def player_form():
-    return render_template("playerform.html")
-
-@www.route("/coaches")
-def coaches():
-    return render_template("coachform.html")
-
-@www.route("/parents")
-def parents():
-    return render_template("parentform.html")
+    return render_template("player_form.html")
 
 @www.route("/contact")
 def contact():
     return render_template("contact.html")
-
-@www.route("/data")
-def data():
-    return render_template("data.html")
-
-@www.route("/histogram")
-def histogram():
-    return render_template("histogram.html")
-
-@www.route("/linegraph")
-def linegraph():
-    return render_template("linegraph.html")
