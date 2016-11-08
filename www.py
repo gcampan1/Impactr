a89389fe17ba665ef7d4d06aa2667cf9d8814974
Brian Kim <bk@breadtech.com>
2016-11-08 08:34:55 -0800

    Begin integrating website with www flask blueprint

--- www.py
+++ www.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 www = Blueprint("www", __name__, template_folder="templates")
 
 @www.route("/")
+def home():
+    return render_template("homepage.html")
+
+@www.route("/players")
 def players():
-    return "home"
+    return render_template("playerform.html")
+
+@www.route("/coaches")
+def coaches():
+    return render_template("coachform.html")
+
+@www.route("/parents")
+def parents():
+    return render_template("parentform.html")
+
